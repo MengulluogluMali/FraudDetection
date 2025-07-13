506dc05ee98349ac2a0a0d3313fc1611c6d22b75
Mehmet Ali <mengulluoglumali@gmail.com>
2025-07-13 17:39:15 +0200

    Week 1

---
+++ fraud detection.py
@@ -0,0 +1,20 @@
+import pandas as pandas
+import numpy as numpy
+import matplotlib.pyplot as matplot
+import seaborn as seabrn
+import os
+
+transactions = pandas.read_csv("C:\\Users\\Monster\\Desktop\\Erasmus 2025\\Internship WSTI\\IEEE\\ieee-fraud-detection\\train_transaction.csv")
+identity = pandas.read_csv("C:\\Users\\Monster\\Desktop\\Erasmus 2025\\Internship WSTI\\IEEE\\ieee-fraud-detection\\train_identity.csv")
+print(transactions.shape)
+print(identity.shape)
+print("\nTransaction columns:\n", transactions.columns.tolist()[:394])  
+print("\nIdentity columns:\n", identity.columns.tolist()[:41])
+print(transactions.head(12))
+print(identity.head(12))
+
+#Adress information is masked because it's a personal infor, instead of deleting this column, i will try to use clustering algorithms and try to mine some useful data out of it. (Creating my own map with multiple clusters)
+#most effective column for fraud marked data should be found using decision tree algorithms.
+
+fraudulent = transactions[transactions['isFraud'] == 1]
+print(fraudulent)
